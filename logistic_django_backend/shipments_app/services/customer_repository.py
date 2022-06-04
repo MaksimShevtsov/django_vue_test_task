23d8027d9409922f5e5a16b06abc044deb502407
xmaksutx <xmaksutx@gmail.com>
2022-06-05 00:58:05 +0200

    changed django response on DRF response

--- logistic_django_backend/shipments_app/services/customer_repository.py
+++ logistic_django_backend/shipments_app/services/customer_repository.py
@@ -1,5 +1,5 @@
 from rest_framework.parsers import JSONParser
-from django.http.response import JsonResponse
+from rest_framework.response import Response
 
 from shipments_app.models import Customer
 from shipments_app.serializers import CustomerSerializer
@@ -8,32 +8,32 @@ from shipments_app.serializers import CustomerSerializer
 class CustomerRepository:
 
     @staticmethod
-    def get_all() -> JsonResponse:
+    def get_all() -> Response:
         customer_data = Customer.objects.all()
         serializer = CustomerSerializer(customer_data, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     @staticmethod
-    def create(request: object) -> JsonResponse:
+    def create(request: object) -> Response:
         customer_data = JSONParser().parse(request)
         customer_data_serializer = CustomerSerializer(data=customer_data)
         if customer_data_serializer.is_valid():
             customer_data_serializer.save()
-            return JsonResponse("Added Successfully", safe=False)
-        return JsonResponse("Failed to Add", safe=False)
+            return Response("Added Successfully")
+        return Response("Failed to Add")
 
     @staticmethod
-    def update(request: object) -> JsonResponse:
+    def update(request: object) -> Response:
         customer_data = JSONParser().parse(request)
         customer_method = Customer.objects.get(customer_id=customer_data['customer_id'])
         customer_methods_serializer = CustomerSerializer(customer_method, data=customer_data)
         if customer_methods_serializer.is_valid():
             customer_methods_serializer.save()
-            return JsonResponse("Updated Successfully", safe=False)
-        return JsonResponse("Failed to Update")
+            return Response("Updated Successfully")
+        return Response("Failed to Update")
 
     @staticmethod
-    def delete(id: int) -> JsonResponse:
+    def delete(id: int) -> Response:
         customer_method = Customer.objects.get(customer_id=id)
         customer_method.delete()
-        return JsonResponse("Deleted Successfully", safe=False)
+        return Response("Deleted Successfully")
--- logistic_django_backend/shipments_app/services/order_repository.py
+++ logistic_django_backend/shipments_app/services/order_repository.py
@@ -1,5 +1,5 @@
 from rest_framework.parsers import JSONParser
-from django.http.response import JsonResponse
+from rest_framework.response import Response
 
 from shipments_app.models import Order
 from shipments_app.serializers import OrderSerializer, OrderPostSerializer
@@ -8,32 +8,32 @@ from shipments_app.serializers import OrderSerializer, OrderPostSerializer
 class OrdersRepository:
 
     @staticmethod
-    def get_all() -> JsonResponse:
+    def get_all() -> Response:
         data = Order.objects.select_related('customer', 'shipping_method')
         serializer = OrderSerializer(data, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     @staticmethod
-    def create(request) -> JsonResponse:
+    def create(request) -> Response:
         data = JSONParser().parse(request)
         data_serializer = OrderPostSerializer(data=data)
         if data_serializer.is_valid():
             data_serializer.save()
-            return JsonResponse("Added Successfully", safe=False)
-        return JsonResponse("Failed to Add", safe=False)
+            return Response("Added Successfully")
+        return Response("Failed to Add")
 
     @staticmethod
-    def update(request) -> JsonResponse:
+    def update(request) -> Response:
         data = JSONParser().parse(request)
         method = Order.objects.get(order_id=data['order_id'])
         order_methods_serializer = OrderPostSerializer(method, data=data)
         if order_methods_serializer.is_valid():
             order_methods_serializer.save()
-            return JsonResponse("Updated Successfully", safe=False)
-        return JsonResponse("Failed to Update")
+            return Response("Updated Successfully")
+        return Response("Failed to Update")
 
     @staticmethod
-    def delete(id: int) -> JsonResponse:
+    def delete(id: int) -> Response:
         method = Order.objects.get(order_id=id)
         method.delete()
-        return JsonResponse("Deleted Successfully", safe=False)
+        return Response("Deleted Successfully")
