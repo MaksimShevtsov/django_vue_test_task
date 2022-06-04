9f00c123f45db5d3ee72b565e8183ab58df9d647
xmaksutx <xmaksutx@gmail.com>
2022-06-05 00:54:01 +0200

    changed django jsonresponse on DRF response

--- logistic_django_backend/shipments_app/services/shippingmethod_repository.py
+++ logistic_django_backend/shipments_app/services/shippingmethod_repository.py
@@ -1,6 +1,5 @@
 from rest_framework.parsers import JSONParser
-from django.http.response import JsonResponse
-
+from rest_framework.response import Response
 from shipments_app.models import ShippingMethod
 from shipments_app.serializers import ShippingMethodSerializer
 
@@ -8,32 +7,32 @@ from shipments_app.serializers import ShippingMethodSerializer
 class ShippingMethodRepository:
 
     @staticmethod
-    def get_all() -> JsonResponse:
+    def get_all() -> Response:
         shipping_methods = ShippingMethod.objects.all()
         shipping_methods_serializer = ShippingMethodSerializer(shipping_methods, many=True)
-        return JsonResponse(shipping_methods_serializer.data, safe=False)
+        return Response(shipping_methods_serializer.data)
 
     @staticmethod
-    def create(request: object) -> JsonResponse:
+    def create(request: object) -> Response:
         shipping_methods_data = JSONParser().parse(request)
         shipping_methods_serializer = ShippingMethodSerializer(data=shipping_methods_data)
         if shipping_methods_serializer.is_valid():
             shipping_methods_serializer.save()
-            return JsonResponse("Added Successfully", safe=False)
-        return JsonResponse("Failed to Add", safe=False)
+            return Response("Added Successfully")
+        return Response("Failed to Add")
 
     @staticmethod
-    def update(request: object) -> JsonResponse:
+    def update(request: object) -> Response:
         shipping_methods_data = JSONParser().parse(request)
         shipping_method = ShippingMethod.objects.get(shipping_method_id=shipping_methods_data['shipping_method_id'])
         shipping_methods_serializer = ShippingMethodSerializer(shipping_method, data=shipping_methods_data)
         if shipping_methods_serializer.is_valid():
             shipping_methods_serializer.save()
-            return JsonResponse("Updated Successfully", safe=False)
-        return JsonResponse("Failed to Update")
+            return Response("Updated Successfully")
+        return Response("Failed to Update")
 
     @staticmethod
-    def delete(id: int) -> JsonResponse:
+    def delete(id: int) -> Response:
         shipping_method = ShippingMethod.objects.get(shipping_method_id=id)
         shipping_method.delete()
-        return JsonResponse("Deleted Successfully", safe=False)
+        return Response("Deleted Successfully")
